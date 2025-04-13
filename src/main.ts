0a5e344bdbfd903ecb501ebaee6e29590a73bac4
Maciej Radzimirski <propiesek@gmail.com>
2025-04-13 21:43:54 +0200

    auto generate swagger schema

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { patchNestJsSwagger } from 'nestjs-zod';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Bootstrap');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,9 +14,25 @@ async function bootstrap() {
   patchNestJsSwagger();
 
   const config = new DocumentBuilder().setTitle('Parking API').build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api/docs', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
+  saveSwaggerSchema(document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
 }
+
+function saveSwaggerSchema(document: OpenAPIObject) {
+  const projectRoot = path.resolve(__dirname, '../..');
+  const outputPath = path.join(projectRoot, 'swagger-schema.json');
+
+  try {
+    fs.writeFileSync(outputPath, JSON.stringify(document, null, 2));
+    logger.log(`Swagger specification successfully generated at ${outputPath}`);
+  } catch (error) {
+    logger.error('Failed to generate Swagger specification file:', error);
+  }
+}
+
 bootstrap();
