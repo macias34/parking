a0ae1f2e32443e391cd67927cc796912f333b9e2
Maciej Radzimirski <propiesek@gmail.com>
2025-04-13 10:23:02 +0200

    add migrations

--- src/common/database.module.ts
+++ src/common/database.module.ts
@@ -1,7 +1,14 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, Logger, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Kysely, PostgresDialect } from 'kysely';
+import {
+  FileMigrationProvider,
+  Kysely,
+  Migrator,
+  PostgresDialect,
+} from 'kysely';
 import { Pool } from 'pg';
+import { promises as fs } from 'fs';
+import * as path from 'path';
 
 export abstract class Database extends Kysely<{}> {}
 
@@ -35,4 +42,59 @@ export abstract class Database extends Kysely<{}> {}
   ],
   exports: [Database],
 })
-export class DatabaseModule {}
+export class DatabaseModule implements OnModuleInit {
+  private readonly logger = new Logger(DatabaseModule.name);
+
+  constructor(private readonly db: Database) {}
+
+  async onModuleInit() {
+    this.logger.log('Running migrations to latest...');
+
+    try {
+      const migrator = new Migrator({
+        db: this.db,
+        provider: new FileMigrationProvider({
+          fs,
+          path,
+          migrationFolder: path.join(process.cwd(), 'migrations'),
+        }),
+      });
+      const { error, results } = await migrator.migrateToLatest();
+
+      results?.forEach((it) => {
+        if (it.status === 'Success') {
+          this.logger.log(
+            `Migration "${it.migrationName}" was executed successfully`,
+          );
+        } else if (it.status === 'Error') {
+          this.logger.error(
+            `Failed to execute migration "${it.migrationName}"`,
+          );
+        } else if (it.status === 'NotExecuted') {
+          this.logger.log(
+            `Migration "${it.migrationName}" was already executed.`,
+          );
+        }
+      });
+
+      if (error) {
+        this.logger.error('Failed to migrate database', error);
+        process.exit(1);
+      } else {
+        const success =
+          results &&
+          results.length > 0 &&
+          results.some((r) => r.status === 'Success');
+
+        if (success) {
+          this.logger.log('Database migrations finished successfully.');
+        } else {
+          this.logger.log('No new database migrations to run.');
+        }
+      }
+    } catch (error) {
+      this.logger.error('Could not run database migrations', error);
+      process.exit(1);
+    }
+  }
+}
