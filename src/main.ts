fc3e67dc4830abb02cee8d5e3c5d50ab5419bf70
Maciej Radzimirski <propiesek@gmail.com>
2025-04-13 21:26:52 +0200

    add api prefix

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { patchNestJsSwagger } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api');
   patchNestJsSwagger();
 
   const config = new DocumentBuilder().setTitle('Parking API').build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('/api/docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
