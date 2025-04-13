ed6e905d91c1f52d920026875bd9fadc747217d6
Maciej Radzimirski <propiesek@gmail.com>
2025-04-13 21:46:31 +0200

    remove generating docs

--- src/main.ts
+++ src/main.ts
@@ -15,24 +15,11 @@ async function bootstrap() {
 
   const config = new DocumentBuilder().setTitle('Parking API').build();
   const document = SwaggerModule.createDocument(app, config);
-  saveSwaggerSchema(document);
 
   SwaggerModule.setup('/api/docs', app, document);
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
-}
-
-function saveSwaggerSchema(document: OpenAPIObject) {
-  const projectRoot = path.resolve(__dirname, '../..');
-  const outputPath = path.join(projectRoot, 'swagger-schema.json');
-
-  try {
-    fs.writeFileSync(outputPath, JSON.stringify(document, null, 2));
-    logger.log(`Swagger specification successfully generated at ${outputPath}`);
-  } catch (error) {
-    logger.error('Failed to generate Swagger specification file:', error);
-  }
 }
 
 bootstrap();
